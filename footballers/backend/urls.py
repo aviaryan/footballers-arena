8eb48a79a40682b3d9085c32748e72f44156aa6b
Avi Aryan <avi.aryan123@gmail.com>
2017-11-18 13:42:39 +0530

    add api to list players

--- footballers/backend/urls.py
+++ footballers/backend/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^players$', views.list_footballers, name='list_footballers')
 ]
--- footballers/backend/views.py
+++ footballers/backend/views.py
@@ -1,5 +1,8 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+import json
+from .models import Footballer
 
 
 def index(request):
@@ -8,3 +11,14 @@ def index(request):
     https://docs.djangoproject.com/en/1.11/intro/tutorial01/
     """
     return HttpResponse("Hello, world. You're at the backend index.")
+
+
+def list_footballers(request):
+    """
+    Returns footballers list in JSON
+    https://docs.djangoproject.com/en/1.11/topics/serialization/
+    """
+    data = serializers.serialize("json", Footballer.objects.all())
+    resp = {'footballers': json.loads(data)}
+    # not sure about json.loads here
+    return JsonResponse(resp)
