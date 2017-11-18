81039c4f2f7e840cdfe95785a42fc1545ad21518
Avi Aryan <avi.aryan123@gmail.com>
2017-11-18 13:52:11 +0530

    register frontend app

--- footballers/footballers/settings.py
+++ footballers/footballers/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'backend.apps.BackendConfig',  # https://docs.djangoproject.com/en/1.11/intro/tutorial02/
+    'frontend.apps.FrontendConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
