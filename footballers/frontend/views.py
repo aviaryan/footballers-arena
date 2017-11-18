4b61816638828f09051ed337a45f30c14eebd952
Avi Aryan <avi.aryan123@gmail.com>
2017-11-18 15:23:56 +0530

    divide layouts into components, add about page

--- footballers/frontend/views.py
+++ footballers/frontend/views.py
@@ -7,3 +7,7 @@ def index(request):
     https://docs.djangoproject.com/en/1.11/intro/tutorial03/
     """
     return render(request, 'index.html', {})
+
+
+def about(request):
+    return render(request, 'about.html', {})
