5942d8c213d887c3b278f8ec9aee6e96ad02d3e1
Avi Aryan <avi.aryan123@gmail.com>
2017-11-18 13:45:16 +0530

    rename backend endpoint to api

--- footballers/footballers/urls.py
+++ footballers/footballers/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^backend/', include('backend.urls')),
+    url(r'^api/', include('backend.urls')),
 ]
