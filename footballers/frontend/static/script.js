568eba0e6555b921a14e20af90ada9c9b0f1e953
Avi Aryan <avi.aryan123@gmail.com>
2017-11-18 15:03:08 +0530

    show data using API working

---
+++ footballers/frontend/static/script.js
@@ -0,0 +1,23 @@
+// parse
+$(document).ready(function(){
+
+	// load initial data
+	$.ajax({
+		type: 'GET',
+		url: '/api/players',
+		dataType: 'json',
+		success: function (data) {
+			var list = data['footballers'];
+			$.each(list, function(index, element) {
+				// console.log(element);
+				var fields = element.fields;
+				$('tbody').append($('<tr>')
+					.append($('<td>').append(element.pk))
+				    .append($('<td>').append(fields.name))
+				    .append($('<td>').append(fields.nationality))
+				    .append($('<td>').append(fields.club))
+				);
+			});
+		}
+	});
+});
